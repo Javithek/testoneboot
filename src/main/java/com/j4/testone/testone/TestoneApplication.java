9cf5b4180fb2c36560c285c43a886176a2759a4d
Javithek <k_inerjavier@hotmail.com>
2023-02-08 12:48:17 -0500

    first commit from intellij

---
+++ src/main/java/com/j4/testone/testone/TestoneApplication.java
@@ -0,0 +1,13 @@
+package com.j4.testone.testone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoneApplication.class, args);
+	}
+
+}
